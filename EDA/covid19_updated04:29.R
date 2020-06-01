9fcfa659523262b465b8bac363223a6c81559b43
JingZhou1 <64068369+JingZhou1@users.noreply.github.com>
2020-06-01 00:24:50 -0700

    Add files via upload

---
+++ EDA/covid19_updated04:29.R
@@ -0,0 +1,51 @@
+setwd('/Users/jing/Desktop/Github')
+# read excel file
+library("readxl")
+patient <- read_excel("confirmed_case.xlsx")
+attach(patient)
+head(patient)
+
+#plot data
+plot(date, Maricopa, type="l",col="red")
+lines(date,Burlington, col="green")
+lines(date, `Los Angeles`, col="yellow")
+lines(date, `New York City`, col="blue")
+
+legend("topleft",
+       c("Maricopa", "Burlington", "Los Angeles", "New York City"),
+       fill=c("red", "green", "yellow", "blue")
+)
+
+#install.packages('easynls')
+library(easynls)
+
+# Step1: Fit data to the gompertz model
+mydata = patient[, 6:7]
+head(mydata)
+mydata <- as.data.frame(mydata) 
+model1 = nlsfit(mydata, model = 10, start = c(a = 100, b = 1, c = .1))
+model1 # represents the number of patients in the given time period t
+
+
+#plot data
+nlsplot(mydata, model = 10, start = c(a = 100, b = 1, c = .1), 
+        xlab = "Date" , ylab = "Patient", position = 1)
+
+
+# Step1: Fit data to the gompertz model
+mydata = patient[, 2:3]
+head(mydata)
+mydata <- as.data.frame(mydata) 
+model1 = nlsfit(mydata, model = 10, start = c(a = 100, b = 1, c = .1))
+model1 # represents the number of patients in the given time period t
+
+
+#plot data
+nlsplot(mydata, model = 10, start = c(a = 100, b = 1, c = .1), 
+        xlab = "Patient" , ylab = "Date", position = 1)
+
+# Step2: Run regression on supply chain factors
+
+
+
+
